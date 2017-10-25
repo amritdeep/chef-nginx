f973b89e021317b0f7218d3f47099a0e2ee33bfa
Amrit Dhungana <amrit.dhungana@ibm.com@amrits-mbp.ustx.ibm.com>
2017-10-25 10:26:10 -0500

    enable nginx and do the test

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,21 +7,21 @@
 package 'epel-release'
 package 'nginx'
 
-execute "chkconfig nginx on" do
-  command "chkconfig nginx on"
-end
-
-execute "service nginx start" do
-  command "/usr/sbin/nginx -k start"
-  action :nothing
-end
-
-# service 'nginx' do
-#   provider Chef::Provider::Service::Systemd
-#   retries 5
-#   retry_delay 10
-#   action [:enable, :start]
+# execute "chkconfig nginx on" do
+#   command "chkconfig nginx on"
 # end
+#
+# execute "service nginx start" do
+#   command "/usr/sbin/nginx -k start"
+#   action :nothing
+# end
+
+service 'nginx' do
+  # provider Chef::Provider::Service::Systemd
+  # retries 5
+  # retry_delay 10
+  action [:enable, :start]
+end
 
 template '/usr/share/nginx/html/index.html' do
   source 'index.html.erb'
--- test/smoke/default/package_test.rb
+++ test/smoke/default/package_test.rb
@@ -4,18 +4,17 @@ end
 
 describe package('nginx') do
   it { should be_installed }
+  it { should be_enabled }
 end
 
-describe command('chkconfig nginx') do
-  its('stdout') { should match('enable') }
-end
-
-# describe service('nginx') do
-#   # it { should be_installed }
-#   # it { should be_enabled }
-#   # it { should be_running }
+# describe command('chkconfig nginx') do
+#   its('stdout') { should match('enable') }
 # end
 
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
 
 describe command('curl localhost') do
   its('stdout') { should match('docker') }
