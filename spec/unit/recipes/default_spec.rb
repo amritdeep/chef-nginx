2205348a0fffd6328bb0f5243927f65f444f5043
Amrit Dhungana <amrit.dhungana@ibm.com@Amrits-MacBook-Pro.local>
2017-10-27 21:54:30 -0500

    make spec for ngnix

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,15 +8,23 @@ require 'spec_helper'
 
 describe 'nginx::default' do
   context 'When all attributes are default, on an Ubuntu 16.04' do
+    packages = ['epel-release', 'nginx']
+
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    packages.each do |pck|
+      it 'should install package' do
+        expect(chef_run).to install_package(pck)
+      end
     end
   end
 end
